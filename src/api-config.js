b45696ac67cae740bdbd0e70a3fdeb135da3748c
Kim, Deokwoo <kdw335552@daum.net>
2024-08-02 06:50:04 +0000

    frontend deployment

--- src/api-config.js
+++ src/api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === "localhost") {
   backendHost = "http://localhost:8080";
 }else {
-  backendHost = "http://dev.kdw335552.p-e.kr";
+  backendHost = "https://dev.kdw335552.p-e.kr";
 }
 
 export const API_BASE_URL = `${backendHost}`;
